6bea686a29d62d3800bd2e5e5e3f66a1f0106c2e
aameli-kalkhoran <159202414+aameli-kalkhoran@users.noreply.github.com>
2024-04-01 13:35:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+###Amin-Farnoosh
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
